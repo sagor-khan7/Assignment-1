1d567a3a4d9bbbfb1ded17cdb43947127f18c20a
sagor khan <sksagor791@gmail.com>
2026-06-27 11:45:12 +0600

    solution 6 completed

--- src/solution.ts
+++ src/solution.ts
@@ -41,3 +41,16 @@ function filterActiveUsers(
 ): { id: number; name: string; email: string; isActive: boolean }[] {
   return items.filter((item) => item.isActive);
 }
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(value: Book): void {
+  console.log(
+    `Title: ${value.title}, Author: ${value.author}, Published: ${value.publishedYear}, Available: ${value.isAvailable ? "Yes" : "No"}`,
+  );
+}
