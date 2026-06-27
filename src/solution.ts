514ecdd8e96d7d7d089a1b8dd86791617e2b4aea
sagor khan <sksagor791@gmail.com>
2026-06-27 11:00:33 +0600

    problem 1 completed

---
+++ src/solution.ts
@@ -0,0 +1,11 @@
+function formatValue(
+  value: string | number | boolean,
+): string | number | boolean {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
+}
