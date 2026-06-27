7c91783aa5870675bee5f84aeb3594fc3f279a38
sagor khan <sksagor791@gmail.com>
2026-06-27 11:29:22 +0600

    solution 4 completed

--- src/solution.ts
+++ src/solution.ts
@@ -29,3 +29,10 @@ class Person {
     return `Name: ${this.name}, Age: ${this.age}`;
   }
 }
+
+function filterByRating(
+  items: { title: string; rating: number }[],
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating > 4);
+}
+
